05e54d9b26229016c6cf7bff81518d4d17e00789
Adam Hardiman <adam@bitrusher.com>
2023-04-08 03:03:55 +1000

    nvim: re-organise lsp mappings
    The vim.buf.lsp.range_code_action function has been removed in neovim
    0.9.0. Removing this mapping has leaves no mappings that aren't using
    that need to use a mode other than normal so I've reduced down to just
    the map method.

--- nvim/lua/plugins/lsp/mappings.lua
+++ nvim/lua/plugins/lsp/mappings.lua
@@ -1,36 +1,32 @@
 local M = {}
 
 function M.setup(_, buffer)
-	local map = function(mode, keys, func, desc)
+	local map = function(keys, func, desc)
 		if desc then
 			desc = "LSP: " .. desc
 		end
 
-		vim.keymap.set(mode, keys, func, { desc = desc, buffer = buffer })
-	end
-	local nmap = function(keys, func, desc)
-		map("n", keys, func, desc)
+		vim.keymap.set("n", keys, func, { desc = desc, buffer = buffer })
 	end
 
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	nmap("<leader>rn", vim.lsp.buf.rename, "Rename")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-	map("v", "<leader>ca", vim.lsp.buf.range_code_action, "Code Action")
+	map("<leader>rn", vim.lsp.buf.rename, "Rename")
+	map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
 
-	nmap("gd", vim.lsp.buf.definition, "Go to definition")
-	nmap("gr", require("telescope.builtin").lsp_references, "Go to references")
-	nmap("gI", vim.lsp.buf.implementation, "Go to implementation")
-	nmap("<leader>D", vim.lsp.buf.type_definition, "Type definition")
-	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "Document symbols")
-	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace symbols")
+	map("gd", vim.lsp.buf.definition, "Go to definition")
+	map("gr", require("telescope.builtin").lsp_references, "Go to references")
+	map("gI", vim.lsp.buf.implementation, "Go to implementation")
+	map("<leader>D", vim.lsp.buf.type_definition, "Type definition")
+	map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "Document symbols")
+	map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace symbols")
 
-	nmap("K", vim.lsp.buf.hover, "Hover documentation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature documentation")
+	map("K", vim.lsp.buf.hover, "Hover documentation")
+	map("<C-k>", vim.lsp.buf.signature_help, "Signature documentation")
 
-	nmap("gD", vim.lsp.buf.declaration, "Go to declaration")
-	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "Workspace: add folder")
-	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Workspace: remove folder")
-	nmap("<leader>wl", function()
+	map("gD", vim.lsp.buf.declaration, "Go to declaration")
+	map("<leader>wa", vim.lsp.buf.add_workspace_folder, "Workspace: add folder")
+	map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Workspace: remove folder")
+	map("<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, "Workspace: list folders")
 end
