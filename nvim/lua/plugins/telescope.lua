0a6d5ad411f29bee298b87d2bd7aecdc8545d82a
Adam Hardiman <adam@bitrusher.com>
2023-06-07 21:50:07 +1000

    nvim: add support for previewing images in telescope using catimg

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,8 +7,39 @@ return {
 	},
 	config = function()
 		local actions = require("telescope.actions")
+
+		local preview_as_image = function(filepath, bufnr, opts)
+			local is_image = function(filepath)
+				local supported_extensions = { "png", "jpg" }
+				local split_path = vim.split(filepath:lower(), ".", { plain = true })
+				local extension = split_path[#split_path]
+				return vim.tbl_contains(supported_extensions, extension)
+			end
+			if is_image(filepath) then
+				local term = vim.api.nvim_open_term(bufnr, {})
+				local function send_output(_, data, _)
+					for _, d in ipairs(data) do
+						vim.api.nvim_chan_send(term, d .. "\r\n")
+					end
+				end
+				vim.fn.jobstart({
+					"catimg",
+					filepath,
+				}, { on_stdout = send_output, stdout_buffered = true, pty = true })
+			else
+				require("telescope.previewers.utils").set_preview_message(
+					bufnr,
+					opts.winid,
+					"Binary cannot be previewed"
+				)
+			end
+		end
+
 		require("telescope").setup({
 			defaults = {
+				preview = {
+					mime_hook = preview_as_image,
+				},
 				layout_config = {
 					prompt_position = "top",
 				},
