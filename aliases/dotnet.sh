38f6f8e4f5477188f0f3e7f527c959f971badc07
Adam Hardiman <adam@bitrusher.com>
2025-02-26 18:50:15 +1000

    scope(aliases): dotnet alias improvements

--- aliases/dotnet.sh
+++ aliases/dotnet.sh
@@ -1,9 +1,13 @@
 dnn() {
-  dotnet new $1 -o $2
+  if [[ $# -eq 2 ]]; then
+    dotnet new "$1" -o "$2"
+  else
+    dotnet new "$@"
+  fi
 }
 
 alias dnr="dotnet run"
 
 dnrp() {
-  dotnet run --project $1
+  dotnet run --project "$@"
 }
